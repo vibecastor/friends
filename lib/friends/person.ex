04dea8491be5426a10245b49c40252501e8c6170
vibecastor <vibecastor@gmail.com>
2019-07-04 21:55:27 -0700

    added changeset and field validation to the schema

--- lib/friends/person.ex
+++ lib/friends/person.ex
@@ -6,4 +6,10 @@ defmodule Friends.Person do
     field(:last_name, :string)
     field(:age, :integer)
   end
+
+  def changeset(person, params \\ %{}) do
+    person
+    |> Ecto.Changeset.cast(params, [:first_name, :last_name, :age])
+    |> Ecto.Changeset.validate_required([:first_name, :last_name])
+  end
 end
